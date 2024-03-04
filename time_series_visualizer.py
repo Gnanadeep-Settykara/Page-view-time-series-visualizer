012daa6f500b08ae37f38748f0029bf84c2a5d0b
Replit user <>
2024-03-04 19:11:09 +0000

    Moving files from replit to GitHub

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,17 +5,22 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
 # Clean data
-df = None
+df = df[
+  (df['value'] >= df['value'].quantile(0.025)) &
+  (df['value'] <= df['value'].quantile(0.975))
+]
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(df.index, df['value'], color='red', linewidth=1 )
+    ax.set_title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Page Views')
 
 
     # Save image and return fig (don't change this part)
@@ -24,10 +29,17 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    dfc = df.copy()
+    dfc['month'] = dfc.index.month
+    dfc['year'] = dfc.index.year
+    df_bar = dfc.groupby(['year','month'])['value'].mean().unstack()
 
     # Draw bar plot
+    fig = df_bar.plot.bar(legend = True, figsize = (12,12), xlabel='Years',ylabel = 'Average Page Views').figure
+    plt.legend(title = 'Months', labels =['January','February','March','April','May','June','July','August','September','October','November','December'], fontsize='medium')
 
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
 
 
 
@@ -44,8 +56,19 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    df_box['month_num'] = df_box['date'].dt.month
+    df_box = df_box.sort_values('month_num')
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
+    axes[0] = sns.boxplot(x=df_box['year'], y=df_box['value'], ax=axes[0])
+    axes[1] = sns.boxplot(x=df_box['month'], y=df_box['value'], ax=axes[1])
 
+    axes[0].set_title('Year-wise Box Plot (Trend)')
+    axes[0].set_xlabel('Year')
+    axes[0].set_ylabel('Page Views')
 
+    axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    axes[1].set_xlabel('Month')
+    axes[1].set_ylabel('Page Views')
 
 
 
